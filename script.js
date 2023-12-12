dc75299d6647114f4af7d1d68aa216a43e54411b
Ben <budemaso@gmail.com>
2023-12-12 12:44:37 -0700

    Added gradually increasing speed of snake movement

--- script.js
+++ script.js
@@ -82,9 +82,11 @@ function move() {
 
     if (head.x === food.x && head.y === food.y) {
         food = generateFood();
-        clearInterval(); // Clear past interval
+        increaseSpeed();
+        clearInterval(gameInterval); // Clear past interval
         gameInterval = setInterval(() => {
             move();
+            // checkCollision();
             draw();
         }, gameSpeedDelay);
     } else {
@@ -136,3 +138,18 @@ function handleKeyPress(event) {
 }
 
 document.addEventListener('keydown', handleKeyPress);
+
+function increaseSpeed() {
+    console.log(gameSpeedDelay);
+    if (gameSpeedDelay > 150) {
+        gameSpeedDelay -= 5;
+    } else if (gameSpeedDelay > 100 ) {
+        gameSpeedDelay -= 3;
+    } else if (gameSpeedDelay > 50 ) {
+        gameSpeedDelay -= 3;
+    } else if (gameSpeedDelay > 25 ) {
+        gameSpeedDelay -= 2;
+    } else if (gameSpeedDelay > 10 ) {
+        gameSpeedDelay -= 1;
+    }
+}
