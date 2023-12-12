1544eb83c9f5ada6ce8cfe3c2821039cec6e639c
Ben <budemaso@gmail.com>
2023-12-11 18:09:13 -0700

    Started creating snake movement

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 const board = document.getElementById('game-board');
 
 // Define game variables
+const gridSize = 20;
 let snake = [{x: 10, y: 10}];
 let food = generateFood();
+let direction = 'right';
 
 // Draw snake, game map, and food
 function draw() {
     board.innerHTML = '';
     drawSnake();
+    drawFood();
 }
 
 // Draw snake
@@ -34,15 +37,31 @@ function setPosition(element, position) {
 }
 
 // Testing draw function
-// draw();
+draw();
 
-// Draw food
+// Draw a food piece on the board
 function drawFood() {
     const foodElement = createGameElement('div', 'food');
-    setPosition(foodElement, food)
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
 }
 
 // Randomly generate food, different position each game load
 function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return { x, y };
+}
 
+// Move the snake, starting at the head and allowing one block of space between the head and the tail
+function move() {
+    const head = { ... snake[0] };
+    switch (direction) {
+        case 'right':
+            head.x++
+            break;
+
+        default:
+            break;
+    }
 }
